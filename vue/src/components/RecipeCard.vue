1dcb84011ee839b3be53762983aab4c0edc2a75d
Adrian Vrbovsky <avrbovsky64@gmail.com>
2023-09-25 10:16:33 +0200

    Added list of recipes

---
+++ vue/src/components/RecipeCard.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import type { Recipe } from '@/types/recipe';
+
+defineProps<{ recipe: Recipe }>();
+</script>
+
+<template>
+  <div class="recipe-card">
+    <div class="card-title">
+      <h1 class="truncate">{{ recipe.title }}</h1>
+      <p>{{ recipe.preparationTime }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.recipe-card {
+  background-image: url('../assets/food-placeholder.png');
+  background-repeat: no-repeat;
+  background-size: auto 100%;
+  background-position: center;
+  height: 250px;
+  position: relative;
+  border: 1px solid lightgray;
+  min-width: 250px;
+  cursor: pointer;
+}
+
+.card-title {
+  padding: 10px;
+  background-color: rgba(0, 0, 0, 0.4);
+  position: absolute;
+  bottom: 0px;
+  width: 100%;
+}
+
+.card-title h1 {
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+}
+
+.card-title p {
+  color: white;
+  font-size: 12px;
+}
+</style>
--- vue/src/views/RecipesView.vue
+++ vue/src/views/RecipesView.vue
@@ -1,3 +1,39 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { RouterLink, useRouter } from 'vue-router';
+
+import type { Recipe } from '@/types/recipe';
+
+import { getRecipes } from '../api/recipes';
+import RecipeCard from '../components/RecipeCard.vue';
+
+const router = useRouter();
+
+const recipes = ref<Recipe[]>([]);
+
+onMounted(async () => {
+  recipes.value = await getRecipes();
+});
+</script>
+
 <template>
-  <div>Recipes view</div>
+  <div>
+    <div class="flex justify-between mb-8">
+      <h2 class="text-4xl text-blue-500">Recepty</h2>
+      <RouterLink
+        to="/novy-recept"
+        class="hover:bg-gray-300 border-solid border px-2 py-1 rounded-2xl text-sm border-gray-400 items-center my-auto"
+      >
+        Novy recept
+      </RouterLink>
+    </div>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <RecipeCard
+        v-for="recipe in recipes"
+        :key="recipe._id"
+        @click="router.push(`/detail/${recipe.slug}`)"
+        :recipe="recipe"
+      />
+    </div>
+  </div>
 </template>
